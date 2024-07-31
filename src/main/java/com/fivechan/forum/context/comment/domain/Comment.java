3e1e31b9e2f2dd23e65e820153d7232ae27e0fef
sergioahmed16 <74843155+sergioahmed16@users.noreply.github.com>
2024-07-31 08:37:18 -0500

    Update Comment.java

--- src/main/java/com/fivechan/forum/context/comment/domain/Comment.java
+++ src/main/java/com/fivechan/forum/context/comment/domain/Comment.java
@@ -1,4 +1,56 @@
 package com.fivechan.forum.context.comment.domain;
 
+import java.util.UUID;
+
 public class Comment {
+    private UUID id;
+    private UUID userId;
+    private UUID topicId;
+    private String content;
+
+    public Comment(UUID id, UUID userId, UUID topicId, String content) {
+        this.id = id;
+        this.userId = userId;
+        this.topicId = topicId;
+        this.content = content;
+    }
+
+    public Comment(UUID userId, UUID topicId, String content) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.topicId = topicId;
+        this.content = content;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public UUID getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(UUID topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
