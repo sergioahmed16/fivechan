1fb1782140521a9d2704146e5ad154c21cad1c3f
sergioahmed16 <74843155+sergioahmed16@users.noreply.github.com>
2024-07-31 09:10:56 -0500

    Create CommentController.java

---
+++ src/main/java/com/fivechan/forum/context/comment/application/CommentController.java
@@ -0,0 +1,46 @@
+package com.fivechan.forum.context.comment.application;
+
+import com.fivechan.forum.context.comment.domain.Comment;
+import com.fivechan.forum.context.comment.domain.CommentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public void createComment(@RequestBody CommentDTO commentDTO) {
+        commentService.createComment(commentDTO);
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments() {
+        return commentService.getAllComments();
+    }
+
+    @GetMapping("/{id}")
+    public Comment getCommentById(@PathVariable UUID id) {
+        return commentService.getCommentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateComment(@PathVariable UUID id, @RequestBody CommentDTO commentDTO) {
+        commentService.updateComment(id, commentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable UUID id) {
+        commentService.deleteComment(id);
+    }
+}
